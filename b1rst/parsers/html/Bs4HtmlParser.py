acdcf43b8eab78f5c4381cb402109a394cc9dbcf
Tomas Brabec <xtbr123@gmail.com>
2024-07-27 14:51:28 +0200

    Inception code for a HTML table handler.

--- b1rst/parsers/html/Bs4HtmlParser.py
+++ b1rst/parsers/html/Bs4HtmlParser.py
@@ -16,6 +16,7 @@
 import bs4
 import docutils.nodes
 import docutils.utils
+import re
 
 class HtmlParser(object):
     """Represents a base class of a HTML parser producing a docutils document tree."""
@@ -54,6 +55,114 @@ class Bs4DefaultHandler(object):
             raise ValueError(f"Cannot handle '<{t}>' elements!")
 
 
+class Bs4TableHandler(object):
+
+    whitespace = re.compile(r'^\s*$')
+
+    def canHandle(self):
+        return ('table', 'tbody', 'tr', 'th', 'td')
+
+
+    def handle(self, element, bs4HtmlParser):
+        t = element.name
+
+        if t in ('table', 'tbody',):
+            # sanity check for 'row' type HTML elements
+            unsupported = [e for e in element.children if self.isUnsupportedTableElement(e)]
+            if len(unsupported) > 0:
+                raise ValueError(f"Unsupported elements under '<{t}>': {unsupported}")
+
+            nodes = []
+            for e in [c for c in element.children if isinstance(c, bs4.Tag)]: nodes.extend(bs4HtmlParser.parseBs4(e))
+
+            if t == 'table' and len(nodes) == 1 and isinstance(nodes[0], docutils.nodes.table):
+                return nodes
+
+            # sanity check for 'row' type doctree subnodes
+            unsupported = [n for n in nodes if not isinstance(n, docutils.nodes.row)]
+            if len(unsupported) > 0:
+                raise ValueError(f"Unsupported doctree subnodes for a table node: {[n.__class__.__name__ for n in unsupported]}")
+
+            cols = 0
+            tbody = docutils.nodes.tbody()
+            for r in nodes:
+                rcols = 0
+                tbody += r
+                for c in r:
+                    if isinstance(c, docutils.nodes.entry):
+                        if 'colspan' in c: rcols += c['colspan']
+                        else: rcols += 1
+
+                if rcols > cols: cols = rcols
+
+            tgroup = docutils.nodes.tgroup(cols=cols)
+            for i in range(0,cols):
+                #TODO temporary until proper `colwidth`
+                #     (using the same value for all columns would assumably yield the even width
+                #     for all columns; using 20 as a "sane" default for writers that may render
+                #     the table in some plain text format)
+                tgroup += docutils.nodes.colspec(colwidth=20)
+            tgroup += tbody
+
+            table = docutils.nodes.table()
+            #TODO temporary until supporting proper `colwidth`
+            #     (using `colwidths-auto` would assumably yield ignoring `colwidth` attributes
+            #     in `colspec` subnodes of `tgroup`)
+            table['classes'] += ['colwidths-auto']
+            table += tgroup
+
+            return [table]
+        elif t == 'tr':
+            # sanity check for 'cell' type HTML elements
+            unsupported = [e for e in element.children if self.isUnsupportedRowElement(e)]
+            if len(unsupported) > 0:
+                raise ValueError(f"Unsupported elements under '<{t}>': {unsupported}")
+
+            nodes = []
+            for e in [c for c in element.children if isinstance(c, bs4.Tag)]: nodes.extend(bs4HtmlParser.parseBs4(e))
+
+            # sanity check for 'cell' type doctree subnodes
+            unsupported = [n for n in nodes if not isinstance(n, docutils.nodes.entry)]
+            if len(unsupported) > 0:
+                raise ValueError(f"Unsupported doctree subnodes for a row node: {[n.__class__.__name__ for n in unsupported]}")
+
+            row = docutils.nodes.row()
+            row.extend(nodes)
+            return [row]
+        elif t in ('td', 'th',):
+            attributes = {}
+            if element.has_attr('rowspan') and int(element['rowspan']) > 1:
+                attributes['morerows'] = int(element['rowspan']) - 1
+            if element.has_attr('colspan') and int(element['colspan']) > 1:
+                attributes['morecols'] = int(element['colspan']) - 1
+            cell = docutils.nodes.entry(**attributes)
+            for e in element.children:
+                for n in bs4HtmlParser.parseBs4(e):
+                    if isinstance(n, docutils.nodes.Text):
+                        p = docutils.nodes.paragraph()
+                        p += n
+                        cell.append(p)
+                    else:
+                        cell.append(n)
+            return [cell]
+        else:
+            raise ValueError(f"Cannot handle '<{t}>' elements!")
+
+
+    def isUnsupportedRowElement(self, e):
+        return (
+                (isinstance(e, bs4.Tag) and e.name not in ('th','td',)) or
+                (isinstance(e, bs4.NavigableString) and Bs4TableHandler.whitespace.match(e.string) is None)
+                )
+
+
+    def isUnsupportedTableElement(self, e):
+        return (
+                (isinstance(e, bs4.Tag) and e.name not in ('tbody','tr',)) or
+                (isinstance(e, bs4.NavigableString) and Bs4TableHandler.whitespace.match(e.string) is None)
+                )
+
+
 class Bs4HtmlParser(HtmlParser):
 
 
@@ -61,9 +170,12 @@ class Bs4HtmlParser(HtmlParser):
         self.opts = opts or self.getDefaultOptions()
         self.handlers = {}
 
-        defaultHandler = Bs4DefaultHandler()
-        for tag in defaultHandler.canHandle():
-            self.handlers[tag] = defaultHandler
+        defaultHandlers = [Bs4TableHandler(),
+                Bs4DefaultHandler(),
+                ]
+        for handler in defaultHandlers:
+            for tag in handler.canHandle():
+                self.handlers[tag] = handler
 
     def parse(self, html):
         parser = None
@@ -75,10 +187,10 @@ class Bs4HtmlParser(HtmlParser):
 
 
     def parseBs4(self, element):
-        """Parses a BeutifulSoup element.
+        """Parses a BeautifulSoup element.
 
         Raise
-            TypeError: When `element` not a bs4 BeutifulSoup type.
+            TypeError: When `element` not a bs4 BeautifulSoup type.
         """
 
         if element is None: return None
@@ -116,6 +228,31 @@ class Bs4HtmlParser(HtmlParser):
 
 parser = Bs4HtmlParser()
 html="""<p>This is <b>some <i>bold</i> test</b> text.</p><p>THis is 2nd paragraph. Here we have <tt>literal</tt> text. Let's see if <tt>literal <b>can</b> contain</tt> some other inline elements.</p>"""
+html="""
+<table>
+<tbody><tr>
+<th colspan="3">2022</th>
+</tr>
+    <tr>
+    <td>&nbsp;</td>
+    <td>&nbsp;</td>
+    <td>&nbsp;  </td>
+</tr>
+<tr>
+<th colspan="2" rowspan="2">FIESTA</th>
+<td>&nbsp;</td>
+</tr>
+<tr>
+<td>&nbsp;</td>
+</tr>
+<tr>
+<td>&nbsp;</td>
+<td>&nbsp;</td>
+<td>&nbsp;</td>
+</tr>
+</tbody>
+</table>
+"""
 document = parser.parse(html)
 print(document.pformat())
 print(20*'=')
@@ -136,4 +273,5 @@ option_parser = docutils.frontend.OptionParser(
 settings = option_parser.parse_args()
 document.settings = settings
 output = writer.write(document, docutils.io.StringOutput(encoding='utf8'))
-print(output)
+soup = bs4.BeautifulSoup(output, 'html.parser')
+print(soup.prettify())
